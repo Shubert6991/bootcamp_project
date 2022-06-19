fb4791087af736e5ea03101640df06fa4e598833
Shubert6991 <alonzoshubert6991@gmail.com>
2022-06-19 00:45:17 -0600

    user scaffold

---
+++ ruby_api/db/migrate/20220619055635_create_users.rb
@@ -0,0 +1,14 @@
+class CreateUsers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :last_name
+      t.string :email
+      t.string :password
+      t.string :address1
+      t.string :address2
+
+      t.timestamps
+    end
+  end
+end
